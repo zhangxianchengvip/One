3357693f82228b577c6be632dfa9a632e4468e33
zhangxiancheng <xian_cheng@ckc.creative.com>
2023-07-21 18:04:54 +0800

    对象存储

---
+++ src/One.ObjectStorage.Aliyun/One/ObjectStorage/Aliyun/IObjectStorageAliyun.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace One.ObjectStorage.Aliyun.One.ObjectStorage.Aliyun
+{
+    public interface IObjectStorageAliyun
+    {
+    }
+}
